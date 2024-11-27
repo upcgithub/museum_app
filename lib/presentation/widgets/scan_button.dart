190ea2e5e29a754177a9ce91a23acb991470db1f
hansleonel <hansleonel@gmail.com>
2024-11-26 20:26:42 -0500

    add scan fixed

--- lib/presentation/widgets/scan_button.dart
+++ lib/presentation/widgets/scan_button.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -8,7 +11,16 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       backgroundColor: Colors.black,
       elevation: 0,
-      onPressed: () async {},
+      onPressed: () async {
+        String barcodeProdResult = await FlutterBarcodeScanner.scanBarcode(
+          '#3D8BEF',
+          'Cancelar',
+          false,
+          ScanMode.QR,
+        );
+
+        log('barcodeProdResult: $barcodeProdResult');
+      },
       child: const Icon(
         Icons.filter_center_focus,
         color: Colors.white,
